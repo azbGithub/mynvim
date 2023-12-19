a6c2153d7d36daf5ee6dfaeec13b814fb121a55d
zhongboai <azbazb011@gmail.com>
2023-12-19 23:28:10 +0800

    update

--- lua/plugins/rainsbow.lua
+++ lua/plugins/rainsbow.lua
@@ -1,17 +1,13 @@
-return{
-"HiPhish/nvim-ts-rainbow2",
+return {
+	"HiPhish/nvim-ts-rainbow2",
 	event = "BufRead",
-	config = function ()
-require('nvim-treesitter.configs').setup {
-  rainbow = {
-    enable = true,
-    -- list of languages you want to disable the plugin for
-    disable = {  },
-    -- Which query to use for finding delimiters
-    query = 'rainbow-parens',
-    -- Highlight the entire buffer all at once
-    strategy = require('ts-rainbow').strategy.global,
-  }
-}
-	end
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			rainbow = {
+				enable = true,
+				query = "rainbow-parens",
+				strategy = require("ts-rainbow").strategy.global,
+			},
+		})
+	end,
 }
