ccabe8b53171413aec1e48189e5aedd9f64720d6
zhongboai <azbazb011@gmail.com>
2023-12-19 23:20:20 +0800

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,10 +7,21 @@ return {
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	opts = {
-		options = {
-			mode = "tabs",
-			separator_style = "slant",
-		},
-	},
+	config = function()
+		require("bufferline").setup({
+			options = {
+				-- 使用 nvim 内置lsp
+				diagnostics = "nvim_lsp",
+				-- 左侧让出 nvim-tree 的位置
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			},
+		})
+	end,
 }
