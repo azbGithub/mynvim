6731df6d995384eb24ba30b03c2604484c165e32
zhongboai <azbazb011@gmail.com>
2023-12-23 17:38:21 +0800

    update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -17,22 +17,20 @@ return {
 			"	 ███╔███╔╝██║  ██║   ██║   ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║	",
 			"	 ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝	",
 			"																																",
-			"																																",
 			"									 No.1  Vim motions														",
 			"								 No.2  Plugin ecosystem													",
 			"							 No.3  Everything is configurable									",
-			"																																",
 			"																																",
 		}
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("e", "  => New File", "<cmd>ene<CR>"),
+			dashboard.button("SPC ee", "  => Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC ff", "󰱼  => Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC fg", "  => Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC wr", "󰁯  => Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  => Quit Nvim", "<cmd>qa<CR>"),
 		}
 
 		dashboard.section.footer.val = {
