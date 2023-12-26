d03c94fe91de446cc4df125995b652e8a0cbbc3e
zhongboai <azbazb011@gmail.com>
2023-12-26 18:48:14 +0800

    fix cmp tab

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,3 +1,8 @@
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 return {
 	"hrsh7th/nvim-cmp",
 	event = { "InsertEnter", "BufRead", "BufNew" },
@@ -10,11 +15,6 @@ return {
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
 	config = function()
-		local has_words_before = function()
-			unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
 		local cmp = require("cmp")
 
 		local luasnip = require("luasnip")
